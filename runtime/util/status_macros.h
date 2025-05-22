03d56435740245733c6f4301f87fcca8484b653b
T.J. Alumbaugh <talumbau@google.com>
2025-05-22 12:16:53 -0700

    LiteRT-LM: Create status_macros target from header
    LiteRT-LM-PiperOrigin-RevId: 762071851

--- runtime/util/status_macros.h
+++ runtime/util/status_macros.h
@@ -4,7 +4,6 @@
 #include <sstream>
 
 #include "absl/status/status.h"  // from @com_google_absl
-#include "litert/cc/litert_macros.h"  // from @litert
 
 // Minimal implementations of status_macros.h and ret_check.h.
 
